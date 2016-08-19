f325db07d577a940ad03b3e388d05ee37a0804c6
mefly2012 <448717571@qq.com>
2016-08-19 16:24:08 +0800

    jyyc

--- src/clean/jyyc.py
+++ src/clean/jyyc.py
@@ -16,7 +16,8 @@ class jyyc():
                          u'remove_date',
                          u'company_name',
                          u'rank_busexcep_date',
-                         u'title'
+                         u'title',
+                         u'decide_docno',
                          ]
 
     def check_regno_or_creditcode(self, source, ustr):
@@ -46,11 +47,32 @@ class jyyc():
     def check_company_name(self, source, ustr):
         """企业名称"""
         ret = None
-        SPECIAL_STR = ur"[ 　.。#＃,，?？/、\`~；;•·$￥@！!^…＇’‘＊*%)(]"
-        if ustr and len(ustr):
-            if re.compile(SPECIAL_STR).search(ustr):
-                ret = u'包含特殊字符'
-        return ret
+        # SPECIAL_STR = ur"[ 　.。#＃,，?？/、\`~；;•·$￥@！!^…＇’‘＊*%)(]"
+        # if ustr and len(ustr):
+        #     if re.compile(SPECIAL_STR).search(ustr):
+        #         ret = u'包含特殊字符'
+        if ustr and len(ustr.strip()):
+            for n, v in enumerate(ustr):
+                if v in u' 　.。#＃,，?？/、\`~；;•·$￥@！!^…＇’‘＊*%':
+                    try:
+                        qian = ustr[n - 1]
+                    except:
+                        qian = ''
+                        pass
+                    try:
+                        hou = ustr[n + 1]
+                    except:
+                        hou = ''
+                    so = qian + hou
+                    if any(c in public.BANJIAO_EN for c in so):
+                        pass
+                    else:
+                        ret = u'前后不是英文字符的特殊符号未清理'
+                        return ret
+            if re.compile(u'[)(]').search(ustr):
+                ret = u'还有半角括号'
+
+            return ret
 
     def check_rank_busexcep_date(self, source, ustr):
         """被列入经营异常名录日期"""
@@ -68,7 +90,15 @@ class jyyc():
                 ret = u'还有半角括号'
         return ret
 
+    def check_decide_docno(self, source, ustr):
+        """被列入经营异常名录日期"""
+        ret = None
+        if ustr and len(ustr):
+            if any(x in ustr for x in u')(][［］'):
+                ret = u'还有半角括号'
+        return ret
+
 
 if __name__ == '__main__':
     a = jyyc()
-    print a.check_regno_or_creditcode(1, u'eee我的')
+    print a.check_company_name(1, u'我。我的')
